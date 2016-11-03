00e7cdbd1fef65e7c5a074fbfa276118a3d88b12
Julia <juliageist23@gmail.com>
2016-11-02 19:01:02 -0700

    solved madlib challenge

---
+++ madlib.py
@@ -0,0 +1,20 @@
+color = raw_input("Color: ")
+
+superlative = raw_input("Superlative, ending in 'est': ")
+
+adjective1 = raw_input("Adjective: ")
+
+bodyPartPlural = raw_input("Body part (plural): ")
+bodyPart = raw_input("Body part: ")
+
+noun1 = raw_input("Noun: ")
+
+animalPlural = raw_input("Animal (plural): ")
+
+adjective2 = raw_input("Adjective: ")
+adjective3 = raw_input("Adjective: ")
+adjective4 = raw_input("Adjective: ")
+
+madLib = "The " + color + "Dragon is the " + superlative + "Dragon of all. It has " + adjective1 + bodyPartPlural + "and a " + bodyPart + "shaped like a " + noun1 + ". It loves to eat " + animalPlural + ", although it will feast on nearly anything. It is " + adjective2 + "and " + adjective3 + ". You must be " + adjective4 + " around it, or you may end up as it`s meal!"
+
+print madLib
