ea492681a5ccd7daeed2b402a6ea8c22aab74790
Julia <juliageist23@gmail.com>
2016-11-04 13:57:52 -0700

    implemented my own reversed-like function

--- reverse.py
+++ reverse.py
@@ -1,6 +1,26 @@
+# import sys
+
+# string = sys.argv[1]
+# listOfCharacters = list(string)
+# reversedList = list(reversed(string))
+# print ''.join(reversedList)
+
 import sys
 
-string = sys.argv[1]
-listOfCharacters = list(string)
-reversedList = list(reversed(string))
-print ''.join(reversedList)
+def reverse(string):
+	listOfCharacters = list(string)
+	numberOfCharacters = string.__len__()
+	highestIndex = numberOfCharacters - 1
+	numberOfIterations = highestIndex / 2
+
+	for i in range(0, numberOfIterations):
+		indexToSwapWith = highestIndex - i
+		listOfCharacters[i], listOfCharacters[indexToSwapWith] = listOfCharacters[indexToSwapWith], listOfCharacters[i]
+
+	return ''.join(listOfCharacters)
+
+
+if __name__ == "__main__":
+	word = sys.argv[1]
+	reversedWord = reverse(word)
+	print reversedWord
