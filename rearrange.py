d456b3a0526275944f135481f7136ae8007cadc7
Julia <juliageist23@gmail.com>
2016-11-04 13:46:44 -0700

    implemented my own random.shuffle-like function

--- rearrange.py
+++ rearrange.py
@@ -1,5 +1,18 @@
-import sys, random
+# import sys, random
+
+# words = sys.argv[1:]
+# random.shuffle(words)
+# print words
+
+import sys
+from random import randint
 
 words = sys.argv[1:]
-random.shuffle(words)
+numberOfWords = words.__len__()
+possibleIndexes = numberOfWords - 1
+
+for i in range(0, possibleIndexes):
+	randomIndex = randint(0, possibleIndexes)
+	words[i] , words[randomIndex] = words[randomIndex], words[i]
+
 print words
