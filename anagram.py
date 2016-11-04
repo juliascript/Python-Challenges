1b23db00433aaae6a428c5505975ac922f4bdd51
Julia <juliageist23@gmail.com>
2016-11-04 14:01:52 -0700

    implemented anagram generator without random.shuffle, but used my own shuffle-like function

--- anagram.py
+++ anagram.py
@@ -1,6 +1,20 @@
-import sys, random
+# import sys, random
 
-string = sys.argv[1]
-listOfCharacters = list(string)
-random.shuffle(listOfCharacters)
-print ''.join(listOfCharacters)
+# string = sys.argv[1]
+# listOfCharacters = list(string)
+# random.shuffle(listOfCharacters)
+# print ''.join(listOfCharacters)
+
+
+import sys, rearrange
+
+def anagram(string):
+	listOfCharacters = list(string)
+	rearrange.shuffle(listOfCharacters)
+	return ''.join(listOfCharacters)
+
+
+if __name__ == '__main__':
+	string = sys.argv[1]
+	anagram = anagram(string)
+	print anagram
