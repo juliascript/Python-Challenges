df034c7a983bf82585f9aa7e42eadef379be1303
Julia <juliageist23@gmail.com>
2016-11-04 21:03:50 -0700

    fix off by 1 error

--- rearrange.py
+++ rearrange.py
@@ -1,3 +1,5 @@
+# in-place shuffle algo 
+
 # import sys, random
 
 # words = sys.argv[1:]
@@ -10,16 +12,16 @@ from random import randint
 # THIS EDITS THE ORIGINAL ARRAY
 def shuffle(array):
 	# count elements in array
-	numberOfElements = array.__len__()
+	numberOfElements = len(array)
 	# number of indexes in array from number of elements in array
 	possibleIndexes = numberOfElements - 1
 
 	# iterate over all indexes
-	for i in range(0, possibleIndexes):
+	for i in range(0, numberOfElements):
 		# generate a random int that is one of the possible indexes
 		randomIndex = randint(0, possibleIndexes)
 		# swap the iterative index and the random index 
-		array[i] , array[randomIndex] = array[randomIndex], array[i]
+		array[i], array[randomIndex] = array[randomIndex], array[i]
 
 # if run from terminal, use the command line args and print the result to the console
 if __name__ == '__main__':
