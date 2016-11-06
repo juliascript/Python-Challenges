78c7bcc3d05d3f5baac9e7ed0b2768046c38d99d
Julia <juliageist23@gmail.com>
2016-11-05 17:35:53 -0700

    generates a random anagram from cmd line arg. implemented using trie algo (with nested dicts). gives false negatives bc shuffling is random.. but never returns false positives.

---
+++ trie.py
@@ -0,0 +1,27 @@
+endOfWord = "$"
+
+def generateTrieFromWordsArray(words):
+	root = {}
+	for word in words:
+		currentDict = root
+		for letter in word:
+			currentDict = currentDict.setdefault(letter, {})
+		currentDict[endOfWord] = endOfWord
+	return root
+
+def isWordPresentInTrie(trie, word):
+	currentDict = trie
+	for letter in word:
+		if letter in currentDict:
+			currentDict = currentDict[letter]
+		else: 
+			return False
+	if endOfWord in currentDict:
+		return True
+	else: 
+		return False 
+
+# arrayOfWords = ['hello', 'hey', 'what', 'when', 'why']
+# wordsTrie = generateTrieFromWordsArray(arrayOfWords)
+# print isWordPresentInTrie(wordsTrie, 'hello')
+# print isWordPresentInTrie(wordsTrie, 'hellow')
