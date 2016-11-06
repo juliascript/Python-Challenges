817bf367a2846c24ab095593fbbf380caf867298
Julia <juliageist23@gmail.com>
2016-11-05 17:35:53 -0700

    generates a random anagram from cmd line arg. implemented using trie algo (with nested dicts). gives false negatives bc shuffling is random.. but never returns false positives.

--- accurateAnagram.py
+++ accurateAnagram.py
@@ -1,30 +1,56 @@
-import sys, rearrange, enchant, math
+import sys, rearrange, trie, math
+
+def generateArrayOfWordsFromWordsTextFile():
+	# open the words file
+	f = open('/usr/share/dict/words', 'r')
+	# read the file, store the string in var called words
+	words = f.read()
+	# create array of words
+	listOfWords = words.split('\n')
+	# close the file
+	f.close()
+	# return list of words
+	return listOfWords
 
 # this returns a new string which is the argument string with its characters shuffled to form a new word
 def shuffleCharactersOf(string):
 	# create a list from the string, each element being a single character in the string
 	listOfCharacters = list(string)
+
 	# using my own shuffle function to rearrange the order of elements in the list of characters
 	rearrange.shuffle(listOfCharacters)
+
 	# return new string
 	return ''.join(listOfCharacters)
 
 # this generates an anagram from the string arg in the language specified. if no lang arg, defaults to US dialect of english
-def generateAnagram(string, language="en_US"):
+def generateAnagram(string):
 	# create dictionary from language
-	languageDict = enchant.Dict(language) 
+	
+	# generate words array
+	wordsArray = generateArrayOfWordsFromWordsTextFile()
+
+	# use my own trie algo to generate trie from words array
+	languageDict = trie.generateTrieFromWordsArray(wordsArray)
+
 	# iterate n! times, where n is the number of characters in the string
 	numberOfPossibleCombinationsForString = math.factorial(len(string))
 	for i in range(0, numberOfPossibleCombinationsForString):
 		# shuffle characters in the string
 		wordWithShuffledCharacters = shuffleCharactersOf(string)
 
-		# if it matches one of the words in the dictionary,
-		if languageDict.check(wordWithShuffledCharacters):
+		# this won't prevent all cases of when the shuffle returns the same order, but it'll shuffle it again
+		if wordWithShuffledCharacters == string:
+			wordWithShuffledCharacters = shuffleCharactersOf(string)
+
+		# use my own trie check algo to check if the word is in the trie
+		if trie.isWordPresentInTrie(languageDict, wordWithShuffledCharacters):
 			# return it
 			return wordWithShuffledCharacters
 	# if the characters were shuffled n! times and didn't get a match, return out with message.
-	return "There is no anagram in %s for %s." % (language, string)
+	# might give a false negative because even though there are n! possibilities, the characters
+	#   are being shuffled randomly and won't always reach every possibility always. 
+	return "There is no anagram for %s." % (string)
 
 # if run from terminal, take first command line arg and attempt to generate anagram, return the result
 if __name__ == '__main__':
