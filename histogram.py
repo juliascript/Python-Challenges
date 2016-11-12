57a2cf42d7e41755a49c388e97333fd64e6c1284
Julia <juliageist23@gmail.com>
2016-11-11 20:55:55 -0800

    generates words based on their occurance in the sample text

--- histogram.py
+++ histogram.py
@@ -1,4 +1,4 @@
-import sys, operator 
+import sys, operator, random, unicodedata
 
 def generateHistogramFromFile(textfile):
 	# open text file 
@@ -7,8 +7,10 @@ def generateHistogramFromFile(textfile):
 	# read the file
 	fileContent = f.read()
 
+	# fileContent = fileContent.encode('utf-8', 'ignore')
+
 	# generate array from file content
-	wordsArray = fileContent.strip().split(' ')
+	wordsArray = fileContent.strip().replace('\n', ' ').split(' ')
 
 	# generate histogram from words array
 	histogram = {}
@@ -18,6 +20,9 @@ def generateHistogramFromFile(textfile):
 		else:
 			histogram[word] = 1
 
+	# function called zip --> tuple
+	# hash function used to implement dictionaries
+	
 	return histogram
 
 def sortHistogram(histogram):
@@ -45,15 +50,32 @@ def createHistogramFileWithName(filename, histogram):
 	for word, count in histogram:
 		f.write("%s %i\n" % (word, count))
 
+def createWeightedListFromHistogram(histogram):
+	_list = []
+	for word, count in histogram:
+		for i in range(count):
+			_list.append(word)
+	return _list
+
+def returnOneWordFromWeightedList(_list):
+	return random.choice(_list)
+
 if __name__ == "__main__":
 	textFile = sys.argv[1]
 	histogram = generateHistogramFromFile(textFile)
 	sortedHistogram = sortHistogram(histogram)
-	print 'leastFrequentWord: ', leastFrequentWordIn(sortedHistogram)
-	print 'mostFrequentWord: ', mostFrequentWordIn(sortedHistogram)
-	print 'numberOfUniqueWords: ', numberOfUniqueWordsIn(sortedHistogram)
-	print 'averageFrequencyOfWords: ', averageFrequencyOfWordsIn(sortedHistogram)
-	createHistogramFileWithName('histogram.txt', sortedHistogram)
-
+	sortedHistogram = sortedHistogram[0:-1]
+	# print sortedHistogram, '\n'
+	# print 'leastFrequentWord: ', leastFrequentWordIn(sortedHistogram)
+	# print 'mostFrequentWord: ', mostFrequentWordIn(sortedHistogram)
+	# print 'numberOfUniqueWords: ', numberOfUniqueWordsIn(sortedHistogram)
+	# print 'averageFrequencyOfWords: ', averageFrequencyOfWordsIn(sortedHistogram)
+	# createHistogramFileWithName('histogram.txt', sortedHistogram)
+	weightedList = createWeightedListFromHistogram(sortedHistogram)
+	# print weightedList
+	sentence = []
+	for i in range(10):
+		sentence.append(returnOneWordFromWeightedList(weightedList))	
+	print sentence
 
 
---
+++ weightedWordGenerator.py
@@ -0,0 +1,49 @@
+import sys, histogram, random, math
+
+
+def generateRandomWordFromHistogram(_histogram):
+	totalWords = 0
+	listOfTuples = []
+	for word, count in _histogram:
+		# add word to tuple with it's range (being it's upper bound)
+		upperBound = totalWords + count
+		listOfTuples.append((word, upperBound))
+		totalWords += count
+
+	# generate random int and then binary search to find the word it correlates to
+	randomInt = random.randint(1, totalWords)
+	word = binarySearch(listOfTuples, randomInt)
+	print word
+	return word
+
+
+def binarySearch(tuples, index):
+	half = math.floor(len(tuples) / 2)
+	midpointIndexOfTuplesList = int(half)
+	if midpointIndexOfTuplesList == 1:
+		print tuples[midpointIndexOfTuplesList][0]
+		return tuples[midpointIndexOfTuplesList][0]
+
+	midpointOfTuplesList = tuples[midpointIndexOfTuplesList]
+
+	lowerBoundOfMidpoint = tuples[midpointIndexOfTuplesList - 1][1]
+	upperBoundOfMidpoint = midpointOfTuplesList[1]
+
+	if lowerBoundOfMidpoint <= index <= upperBoundOfMidpoint:
+		print tuples[midpointIndexOfTuplesList][0]
+		return tuples[midpointIndexOfTuplesList][0]
+	elif index < lowerBoundOfMidpoint:
+		upperBound = midpointIndexOfTuplesList - 1
+		binarySearch(tuples[0:upperBound], index)
+	elif upperBoundOfMidpoint < index:
+		upperBound = midpointIndexOfTuplesList - 1
+		binarySearch(tuples[midpointIndexOfTuplesList:-1], index)
+
+
+
+if __name__ == "__main__":
+	_file = sys.argv[1]
+	_histogram = histogram.generateHistogramFromFile(_file)
+	sortedHistogram = histogram.sortHistogram(_histogram)
+	# print sortedHistogram
+	print generateRandomWordFromHistogram(sortedHistogram)
