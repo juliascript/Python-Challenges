131b4663962855f33c6fb8275fc2cc9e7c1af228
Julia <juliageist23@gmail.com>
2016-11-14 17:33:46 -0800

    fixed None return error, it was bc of recursion

--- weightedWordGenerator.py
+++ weightedWordGenerator.py
@@ -12,7 +12,6 @@ def generateRandomWordFromHistogram(_histogram):
 	# generate random int and then binary search to find the word it correlates to
 	randomInt = random.randint(1, totalWords)
 	word = binarySearch(listOfTuples, randomInt)
-	print 'returned:', word
 	return word
 
 def binarySearch(tuples, index):
@@ -20,7 +19,6 @@ def binarySearch(tuples, index):
 	midpointIndexOfTuplesList = int(half)
 	if midpointIndexOfTuplesList == 1:
 		word = tuples[midpointIndexOfTuplesList][0]
-		print 'about to return', word
 		return word
 
 	midpointOfTuplesList = tuples[midpointIndexOfTuplesList]
@@ -29,21 +27,18 @@ def binarySearch(tuples, index):
 	upperBoundOfMidpoint = midpointOfTuplesList[1]
 
 	if lowerBoundOfMidpoint <= index <= upperBoundOfMidpoint:
-		print 'about to return', word
 		return tuples[midpointIndexOfTuplesList][0]
 	elif index < lowerBoundOfMidpoint:
 		upperBound = midpointIndexOfTuplesList - 1
-		binarySearch(tuples[0:upperBound], index)
+		return binarySearch(tuples[0:upperBound], index)
 	elif upperBoundOfMidpoint < index:
 		upperBound = midpointIndexOfTuplesList - 1
-		binarySearch(tuples[midpointIndexOfTuplesList:-1], index)
+		return binarySearch(tuples[midpointIndexOfTuplesList:-1], index)
 
 
 def generateSentenceFromTextfile(textfile):
 	_histogram = histogram.generateHistogramFromFile(textfile)
 	sortedHistogram = histogram.sortHistogram(_histogram)
-	# print sortedHistogram
-	print generateRandomWordFromHistogram(sortedHistogram)
 
 	sentence = []
 	for i in range(7):
